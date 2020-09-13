6b65250b71a56b40e8d810c549b005cde7cda4a0
Łukasz Latała <lukaszlatala@MacBook-Pro-ukasz.local>
2020-09-13 22:12:26 +0200

    search option added

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-// pobrane elementy
+// Pobrane elementy
 const todosWrapper = document.querySelector("ul.add");
 const addTodoBtn = document.querySelector(".todo-form__button");
 const addTodoInput = document.querySelector(".todo-form__input");
-
 const listItems = document.getElementsByClassName("task");
-// funkcja
+const searchInput = document.querySelector(".searchInput");
+const ulSearch = document.querySelector("ul.search");
 
+const toDoList = [];
+
+// Funkcja usuwająca taska
 const removeTask = (e) => {
   e.target.parentNode.remove();
-};
+  const index = e.target.parentNode.id.key;
 
+  toDoList.splice(index, 1);
+  console.log(index);
+  todosWrapper.textContent = "";
+  toDoList.forEach((todoElement, key) => {
+    todoElement.id = key;
+    todosWrapper.appendChild(todoElement);
+  });
+};
+// Funkcja dodająca taska
 const addItems = (e) => {
   e.preventDefault();
 
@@ -27,8 +39,35 @@ const addItems = (e) => {
   todosWrapper.appendChild(newTask);
   newTask.appendChild(btn);
   addTodoInput.value = "";
-  // nasluchiwanie
+
   btn.addEventListener("click", removeTask);
+
+  toDoList.push(newTask);
+  todosWrapper.textContent = "";
+  toDoList.forEach((todoElement, key) => {
+    todoElement.id = key;
+    todosWrapper.appendChild(todoElement);
+  });
 };
-// nasluchiwanie
+// Funckja wyszukująca taska
+const searchTask = (e) => {
+  ulSearch.textContent = "";
+
+  const matchingTasks = toDoList.filter((newTask) =>
+    newTask.firstChild.nodeValue
+      .toLowerCase()
+      .includes(e.target.value.toLowerCase())
+  );
+
+  matchingTasks.forEach((newTask) => {
+    const newEl = document.createElement("li");
+    newEl.textContent = newTask.firstChild.nodeValue;
+    ulSearch.appendChild(newEl);
+  });
+
+  if (e.target.value === "") ulSearch.textContent = "";
+};
+
 addTodoBtn.addEventListener("click", addItems);
+
+searchInput.addEventListener("input", searchTask);
